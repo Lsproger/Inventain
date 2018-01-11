aa685c1355203c76918d0967f9ef790b7c7527f0
lsproger <yur4iik@tut.by>
2018-01-12 02:16:49 +0300

    Start of the project
    There are some prefabs, animation, needed textures

---
+++ My Stick Hero/Assets/Scripts/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+    internal bool isSoundOn = true;
+    internal bool isPlayButtonClicked = false;
+    internal Image soundImage;
+
+
+    [SerializeField]
+    private Sprite sndOnImg;
+    [SerializeField]
+    private Sprite sndOffImg;
+    [SerializeField]
+    private Canvas canvas;
+    [SerializeField]
+    private GameObject character;
+
+
+    void Start()
+    {
+        transform.Find("Play").GetComponent<Button>().onClick.
+            AddListener(delegate () { PlayButton_OnCLick(); });
+        transform.Find("Sound").GetComponent<Button>().onClick.
+            AddListener(delegate () { SoundButton_OnClick(); });
+        soundImage = transform.Find("Sound").GetComponent<Image>();
+        soundImage.sprite = sndOnImg;
+    }
+
+
+    void FixedUpdate()
+    {
+
+        if (character.GetComponent<Animator>().GetBool("isWalking"))
+        {
+            Debug.Log("WALKING");
+            Vector3 currCharPos = character.transform.position;
+            character.transform.position = new Vector3((currCharPos.x + 0.1f) * Time.deltaTime, currCharPos.y, currCharPos.z);
+            Camera.main.transform.Translate(new Vector3(0.1f, 0, 0) * Time.deltaTime);
+        }
+
+    }
+
+    private void PlayButton_OnCLick()
+    {
+        canvas.transform.Find("Menu").gameObject.SetActive(false);
+        canvas.transform.Find("Game").gameObject.SetActive(true);
+        character.GetComponent<Animator>().SetBool("isWalking", true);
+        Debug.Log(character.GetComponent<Animator>().GetBool("isWalking"));
+    }
+
+
+    private void SoundButton_OnClick()
+    {
+        if (isSoundOn)
+        {
+            AudioListener.volume = 0;
+            soundImage.sprite = sndOffImg;
+            isSoundOn = !isSoundOn;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            soundImage.sprite = sndOnImg;
+            isSoundOn = !isSoundOn;
+        }
+    }
+}
---
+++ My Stick Hero/Assets/Scripts/PlatformEdge.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    void OnTriggerEnter2d(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            coll.GetComponentInParent<Animator>().SetBool("isWalking", false);
+            coll.gameObject.SetActive(false);
+        }
+        
+    }
+}
